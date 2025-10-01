38484051296dbc10bdf701ea8b10ab4863871d65
alexey <eskealexey@mail.ru>
2025-10-01 23:09:29 +0300

    обработка файла xls и получение JSON файлов - общий необработтаный и обработанный ОСФР

---
+++ class_tel_spr.py
@@ -0,0 +1,102 @@
+import json
+import os
+
+import pandas as pd
+
+
+class ParserXls:
+    def __init__(self, filexls):
+        if not os.path.exists(filexls):
+            raise FileNotFoundError(f"Файл {filexls} не найден")
+        self.filexls = filexls
+        self.all_data = {}
+
+
+    def __str__(self):
+        return self.filexls
+
+    def parser(self, output_dir="JSON", output_file="all_sheets.json"):
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        excel_file = pd.ExcelFile(self.filexls)
+        sheet_names = excel_file.sheet_names
+
+        all_data = {}
+
+        for sheet_name in sheet_names:
+            df = pd.read_excel(self.filexls, sheet_name=sheet_name)
+
+            # Замена NaN на пустые строки для корректного JSON
+            df = df.fillna('')
+
+            # Сохраняем данные листа как список словарей
+            all_data[sheet_name] = df.to_dict(orient='records')
+
+        json_path = os.path.join(output_dir, output_file)
+
+        with open(json_path, 'w', encoding='utf-8') as f:
+            json.dump(all_data, f, ensure_ascii=False, indent=2)
+
+        self.all_data = all_data
+
+        return self.all_data
+
+
+class SprOsfr(ParserXls):
+    """
+    Класс для обработки Excel-файла с выделением нужных листов.
+    """
+    target_sheets = ["ОСФР"]  # Листы, которые нужно обработать
+    target_map = {
+        'Unnamed: 0': 'Городской номер',
+        'Unnamed: 1': 'Кор. тел.',
+        'Unnamed: 2': '№ комн.',
+        'Unnamed: 3': 'ФАМИЛИЯ',
+        'Unnamed: 4': 'ИМЯ',
+        'Unnamed: 5': 'ОТЧЕСТВО',
+        'Unnamed: 6': 'ДОЛЖНОСТЬ',
+        'Unnamed: 7': 'Отдел',
+        'Unnamed: 8': 'Место расположения'
+    }
+
+    def __init__(self, filexls):
+        super().__init__(filexls)
+        self.osfr = None
+
+    def obrabotka_osfr(self):
+        list_strok = []
+        data = self.parser()
+        for sheet_name in data.keys():
+            if sheet_name in self.target_sheets:
+                # print(sheet_name)
+                for row in data[sheet_name]:
+                    if row.get("Unnamed: 8", '') != '':
+                        # Создаем новую строку с замененными ключами
+                        new_row = {}
+                        for old_key, value in row.items():
+                            # Если ключ есть в target_map, используем новое название, иначе оставляем старый
+                            new_key = self.target_map.get(old_key, old_key)
+                            new_row[new_key] = value
+                        list_strok.append(new_row)
+        if len(list_strok) > 1:
+            list_strok_ = list_strok[1:]
+        else:
+            list_strok_ = list_strok
+
+        return list_strok_
+
+    def save_to_json(self, output_dir="JSON", output_file='osfr.json'):
+        """
+        Сохраняет результат obrabotka_osfr в JSON файл
+        """
+        json_path = os.path.join(output_dir, output_file)
+        result = self.obrabotka_osfr()
+        with open(json_path, 'w', encoding='utf-8') as f:
+            json.dump(result, f, ensure_ascii=False, indent=4)
+        print(f"Данные успешно записаны в {json_path}")
+
+
+if __name__ == '__main__':
+    xls = SprOsfr("telef.xls")
+    xls.save_to_json()
